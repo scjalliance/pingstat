7683755d21d1db0658a56bcbb9d927efa1ef3e79
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2018-11-12 18:28:04 -0800

    Support for custom ping flags and units of measure from ping output

--- pingstat.sh
+++ pingstat.sh
@@ -53,25 +53,60 @@ if [[ "$PEERINSTANCE" = "${INSTANCE}-"* ]]; then
 	exit 0
 fi
 
+: "${PING_FLAGS:=-n -c60 -i1 -W1}"
+
 STATNAME="netstat $INSTANCE $PEERINSTANCE"
 
 #60 packets transmitted, 60 received, 0% packet loss, time 59093ms
 #rtt min/avg/max/mdev = 20.200/32.525/55.603/6.642 ms
 
-STAT=$(ping -c2 -i1 -W1 "$PEERADDR" | tail -n2)
+STAT=$(ping $PING_FLAGS "$PEERADDR" | tail -n2)
 
-SENT=$(sed -n 's/.*\([0-9][0-9]*\)\s*packets.*/\1/p' <<<"$STAT")
-RCVD=$(sed -n 's/.*\([0-9][0-9]*\)\s*received.*/\1/p' <<<"$STAT")
-LOSS=$(sed -n 's/.*\([0-9][0-9]*\)%\s*packet\s*loss.*/\1/p' <<<"$STAT")
+SENT=$(sed -n 's/^\([0-9][0-9]*\)\s*packets.*/\1/p' <<<"$STAT")
+RCVD=$(sed -n 's/.*, *\([0-9][0-9]*\)\s*received.*/\1/p' <<<"$STAT")
+LOSS=$(sed -n 's/.*, *\([0-9][0-9]*\)%\s*packet\s*loss.*/\1/p' <<<"$STAT")
 RTT=$(sed -n 's/.*=\s*\([0-9\./][0-9\./]*\)\s*.*/\1/p' <<<"$STAT")
 RTTMIN=$(cut -d/ -f1 <<<"$RTT")
 RTTAVG=$(cut -d/ -f2 <<<"$RTT")
 RTTMAX=$(cut -d/ -f3 <<<"$RTT")
-RTTMDEV=$(cut -d/ -f4 <<<"$RTT")
-RTTJITTER=$(bc <<<"${RTTMAX} - ${RTTMIN}")
+RTTUNIT=$(sed -n 's/.* \(\(ns\)*\(ms\)*\(s\(ec\)*\)*\(m\(in\)*\)*\)$/\1/p' <<<"$STAT")
 
-curl -d "stat=${STATNAME} loss&ezkey=${STATHAT_EZKEY}&value=${LOSS}" -m 10 https://api.stathat.com/ez
-curl -d "stat=${STATNAME} min&ezkey=${STATHAT_EZKEY}&value=${RTTMIN}" -m 10 https://api.stathat.com/ez
-curl -d "stat=${STATNAME} avg&ezkey=${STATHAT_EZKEY}&value=${RTTAVG}" -m 10 https://api.stathat.com/ez
-curl -d "stat=${STATNAME} max&ezkey=${STATHAT_EZKEY}&value=${RTTMAX}" -m 10 https://api.stathat.com/ez
-curl -d "stat=${STATNAME} jitter&ezkey=${STATHAT_EZKEY}&value=${RTTJITTER}" -m 10 https://api.stathat.com/ez
+if [ "$RTTUNIT" = "ns" ]; then
+	RTTMIN=0
+	RTTAVG=0
+	RTTMAX=0
+fi
+
+if [ "$RTTUNIT" = "s" -o "$RTTUNIT" = "sec" ]; then
+	RTTMIN=$(bc <<<"scale=3; ${RTTMIN} * 1000")
+	RTTAVG=$(bc <<<"scale=3; ${RTTAVG} * 1000")
+	RTTMAX=$(bc <<<"scale=3; ${RTTMAX} * 1000")
+fi
+
+if [ "$RTTUNIT" = "m" -o "$RTTUNIT" = "min" ]; then
+	RTTMIN=$(bc <<<"scale=3; ${RTTMIN} * 1000000")
+	RTTAVG=$(bc <<<"scale=3; ${RTTAVG} * 1000000")
+	RTTMAX=$(bc <<<"scale=3; ${RTTMAX} * 1000000")
+fi
+
+RTTJITTER=$(bc <<<"scale=3; ${RTTMAX} - ${RTTMIN}")
+
+if [ ! -z "$LOSS" ]; then
+	curl -d "stat=${STATNAME} loss&ezkey=${STATHAT_EZKEY}&value=${LOSS}" -m 10 https://api.stathat.com/ez
+fi
+
+if [ ! -z "$RTTMIN" ]; then
+	curl -d "stat=${STATNAME} min&ezkey=${STATHAT_EZKEY}&value=${RTTMIN}" -m 10 https://api.stathat.com/ez
+fi
+
+if [ ! -z "$RTTAVG" ]; then
+	curl -d "stat=${STATNAME} avg&ezkey=${STATHAT_EZKEY}&value=${RTTAVG}" -m 10 https://api.stathat.com/ez
+fi
+
+if [ ! -z "$RTTMAX" ]; then
+	curl -d "stat=${STATNAME} max&ezkey=${STATHAT_EZKEY}&value=${RTTMAX}" -m 10 https://api.stathat.com/ez
+fi
+
+if [ ! -z "$RTTJITTER" ]; then
+	curl -d "stat=${STATNAME} jitter&ezkey=${STATHAT_EZKEY}&value=${RTTJITTER}" -m 10 https://api.stathat.com/ez
+fi
